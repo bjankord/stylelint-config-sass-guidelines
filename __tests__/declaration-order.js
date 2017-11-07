8ee657b7767ce88a6ecaa579856a0168e0bf33b8
Jeff <rustyangel@gmail.com>
2017-11-07 11:42:38 -0700

    Add rules & tests for declaration-order
    Should allow stylelint to enforce ordering, a la, scss-lint (see: https://github.com/bjankord/stylelint-config-sass-guidelines/wiki/Lint-Report-Comparison#declaration-order-details).

--- __tests__/declaration-order.js
+++ __tests__/declaration-order.js
@@ -1,11 +1,10 @@
-import config from "../"
-import stylelint from "stylelint"
-import postcss from "postcss"
-import scssSyntax from "postcss-scss"
-import test from "tape"
+import config from '../'
+import stylelint from 'stylelint'
+import postcss from 'postcss'
+import scssSyntax from 'postcss-scss'
+import test from 'tape'
 
-const invalidScss = (
-`// scss features not lintable for declaration order in stylelint
+const invalidScss = `// scss features not lintable for declaration order in stylelint
 .declarationorder {
   p {
     color: #f00;
@@ -17,20 +16,40 @@ const invalidScss = (
   @extend %error;
 }
 
-`)
+`
 
-test("Declaration order scss", t => {
-  t.plan(2)
+test('Declaration order scss', t => {
+  t.plan(5)
 
   postcss()
-    .use(stylelint({ code: invalidScss, config: config,}))
+    .use(stylelint({ code: invalidScss, config: config }))
     .process(invalidScss, { syntax: scssSyntax })
     .then(checkResult)
     .catch(logError)
 
   function checkResult(result) {
-    t.equal(result.warnings().length, 1, "flags 1 warning")
-    t.is(result.warnings()[0].text, "Expected background-color to come before color (order/properties-alphabetical-order)", "correct warning text")
+    console.log(result)
+    t.equal(result.warnings().length, 4, 'flags 4 warnings')
+    t.is(
+      result.warnings()[0].text,
+      'Expected declaration to come before rule (order/order)',
+      'correct warning text',
+    )
+    t.is(
+      result.warnings()[1].text,
+      'Expected blockless @include to come before declaration (order/order)',
+      'correct warning text',
+    )
+    t.is(
+      result.warnings()[2].text,
+      'Expected @extend to come before blockless @include (order/order)',
+      'correct warning text',
+    )
+    t.is(
+      result.warnings()[3].text,
+      'Expected background-color to come before color (order/properties-alphabetical-order)',
+      'correct warning text',
+    )
   }
 })
 
