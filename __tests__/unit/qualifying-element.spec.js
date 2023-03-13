a742dc1659090886d262f0d5bf82ea28c5b29e83
Brett Jankord <bjankord@gmail.com>
2023-03-13 18:25:41 -0500

    Stylelint v15 update (#276)
    * Update npm dependencies
    
    * Remove deprecated stylelint rules
    
    * Drop support for node 12 and add support for node 18

--- __tests__/unit/qualifying-element.spec.js
+++ __tests__/unit/qualifying-element.spec.js
@@ -31,14 +31,14 @@ test("Qualifying element scss", t => {
     .then(checkResult)
     .catch(logError)
 
-  function checkResult(result) {
-    t.equal(result.warnings().length, 5, "flags 5 warning")
-    t.is(result.warnings()[0].text, "Expected \"div#thing\" to have no more than 0 ID selectors (selector-max-id)", "correct warning text")
-    t.is(result.warnings()[1].text, "Unexpected qualifying type selector (selector-no-qualifying-type)", "correct warning text")
-    t.is(result.warnings()[2].text, "Unexpected qualifying type selector (selector-no-qualifying-type)", "correct warning text")
-    t.is(result.warnings()[3].text, "Unexpected qualifying type selector (selector-no-qualifying-type)", "correct warning text")
-    t.is(result.warnings()[4].text, "Unexpected qualifying type selector (selector-no-qualifying-type)", "correct warning text")
-  }
+    function checkResult(result) {
+      t.equal(result.warnings().length, 5, "flags 5 warning")
+      t.is(result.warnings()[0].text, "Expected \"div#thing\" to have no more than 0 ID selectors (selector-max-id)", "correct warning text")
+      t.is(result.warnings()[1].text, "Unexpected qualifying type selector \"div#thing\" (selector-no-qualifying-type)", "correct warning text")
+      t.is(result.warnings()[2].text, "Unexpected qualifying type selector \"ul.list\" (selector-no-qualifying-type)", "correct warning text")
+      t.is(result.warnings()[3].text, "Unexpected qualifying type selector \"li.item\" (selector-no-qualifying-type)", "correct warning text")
+      t.is(result.warnings()[4].text, "Unexpected qualifying type selector \"a[href=\'place\']\" (selector-no-qualifying-type)", "correct warning text")
+    }
 })
 
 function logError(err) {
--- index.js
+++ index.js
@@ -6,19 +6,10 @@ module.exports = {
     "at-rule-no-unknown": null,
     "at-rule-no-vendor-prefix": true,
     "block-no-empty": true,
-    "block-opening-brace-space-before": "always",
-    "color-hex-case": "lower",
     "color-hex-length": "short",
     "color-named": "never",
     "color-no-invalid-hex": true,
-    "declaration-bang-space-after": "never",
-    "declaration-bang-space-before": "always",
-    "declaration-block-semicolon-newline-after": "always",
-    "declaration-block-semicolon-space-before": "never",
     "declaration-block-single-line-max-declarations": 1,
-    "declaration-block-trailing-semicolon": "always",
-    "declaration-colon-space-after": "always-single-line",
-    "declaration-colon-space-before": "never",
     "declaration-property-value-disallowed-list": {
       "border": ["none"],
       "border-top": ["none"],
@@ -26,10 +17,7 @@ module.exports = {
       "border-bottom": ["none"],
       "border-left": ["none"]
     },
-    "function-comma-space-after": "always-single-line",
-    "function-parentheses-space-inside": "never",
     "function-url-quotes": "always",
-    "indentation": 2,
     "length-zero-no-unit": true,
     "max-nesting-depth": [
       1,
@@ -43,10 +31,6 @@ module.exports = {
       }
     ],
     "media-feature-name-no-vendor-prefix": true,
-    "media-feature-parentheses-space-inside": "never",
-    "no-missing-end-of-source-newline": true,
-    "number-leading-zero": "always",
-    "number-no-trailing-zeros": true,
     "order/order": [
       [
         "custom-properties",
@@ -97,7 +81,6 @@ module.exports = {
           "Selector should be written in lowercase with hyphens (selector-class-pattern)"
       }
     ],
-    "selector-list-comma-newline-after": "always",
     "selector-max-compound-selectors": 3,
     "selector-max-id": 0,
     "selector-no-qualifying-type": true,
@@ -105,7 +88,6 @@ module.exports = {
     "selector-pseudo-element-colon-notation": "double",
     "selector-pseudo-element-no-unknown": true,
     "shorthand-property-no-redundant-values": true,
-    "string-quotes": "single",
     "value-no-vendor-prefix": true
   }
 }
