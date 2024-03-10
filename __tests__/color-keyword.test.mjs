9aa4cb3de9f219ebc5776348795189c7fa7f6c77
Brett <bjankord@gmail.com>
2024-03-10 17:10:09 -0500

    Modernize unit tests

---
+++ __tests__/color-keyword.test.mjs
@@ -0,0 +1,49 @@
+import { beforeEach, describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+
+import stylelint from 'stylelint';
+
+import config from '../index.js';
+
+describe('flags warnings with named color keyword', () => {
+	const invalidScss = (
+`.colorkeyword {
+	color: green;
+}
+`);
+
+	let result;
+
+	beforeEach(async () => {
+		result = await stylelint.lint({
+			code: invalidScss,
+			config,
+		});
+	});
+
+	it('did error', () => {
+		assert.equal(result.errored, true);
+	});
+
+	it('flags warnings', () => {
+		assert.equal(result.results[0].warnings.length, 1);
+	});
+
+	it('correct warning text', () => {
+		assert.deepEqual(
+			result.results[0].warnings.map((w) => w.text),
+			[
+				'Unexpected named color "green" (color-named)',
+			],
+		);
+	});
+
+	it('correct rule flagged', () => {
+		assert.deepEqual(
+			result.results[0].warnings.map((w) => w.rule),
+			[
+				'color-named',
+			],
+		);
+	});
+});
---
+++ __tests__/url-quotes.test.mjs
@@ -0,0 +1,49 @@
+import { beforeEach, describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+
+import stylelint from 'stylelint';
+
+import config from '../index.js';
+
+describe('flags warnings with missing url quotes', () => {
+	const invalidScss = (
+`.quotes-url {
+  background: url(example.png);
+}
+`);
+
+	let result;
+
+	beforeEach(async () => {
+		result = await stylelint.lint({
+			code: invalidScss,
+			config,
+		});
+	});
+
+	it('did error', () => {
+		assert.equal(result.errored, true);
+	});
+
+	it('flags warnings', () => {
+		assert.equal(result.results[0].warnings.length, 1);
+	});
+
+	it('correct warning text', () => {
+		assert.deepEqual(
+			result.results[0].warnings.map((w) => w.text),
+			[
+				'Expected quotes around "url" function argument (function-url-quotes)',
+			],
+		);
+	});
+
+	it('correct rule flagged', () => {
+		assert.deepEqual(
+			result.results[0].warnings.map((w) => w.rule),
+			[
+				'function-url-quotes',
+			],
+		);
+	});
+});
